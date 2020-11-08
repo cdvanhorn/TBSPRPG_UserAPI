5e2770ced308688d7d5a94c9212d76f4685258d9
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-11-08 15:59:25 -0500

    Game Service
    Updated users schema.

--- UserApi/Entities/User.cs
+++ UserApi/Entities/User.cs
@@ -9,7 +9,6 @@ namespace UserApi.Entities {
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Username { get; set; }
-        public string Email { get; set; }
 
         [JsonIgnore]
         public string Password { get; set; }
